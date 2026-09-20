3f07588dee32e85e1218526e16477da7cb4f0c48
maxiliano kuntzius <maximilianokuntzius@gmail.com>
2026-09-20 13:16:06 -0300

    Agrego script de bucles.

---
+++ mis-scripts/script-5-contador.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Crear un script (puedes llamarlo contador.sh) que cuente del 1 al 5 automáticamente en la pantalla, imprimiendo:
+#Contando: 1
+#Contando: 2
+#(...hasta llegar al 5)
+for numero in 1 2 3 4 5;do
+echo "Contando:$numero"
+done
